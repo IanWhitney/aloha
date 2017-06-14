9e0abe5b1f0e95da5bc65bf26ab257690b35c5ed
Ian Whitney <whit0694@umn.edu>
2017-06-14 17:29:07 -0500

    fixx

--- lib/aloha/hooks/check_for_updates.rb
+++ lib/aloha/hooks/check_for_updates.rb
@@ -8,7 +8,7 @@ module Aloha
       REMOTE_VERSION_JSON_URL = "https://raw.githubusercontent.com/ftwnyc/aloha/update-notifications/config/version.json"
       def call client, data
         return if data.presence != 'active'
-        slack_user = Aloha::SlackUser.find(client, data).name
+        slack_user = Aloha::SlackUser.find(client, data)
         return if slack_user.name == client.name
 
         user = User.find_by(slack_id: slack_user.id)
