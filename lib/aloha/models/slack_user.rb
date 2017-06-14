b514463fa72ad4e2607008e44e512922f9959d37
Ian Whitney <whit0694@umn.edu>
2017-06-14 17:11:34 -0500

    debug

--- lib/aloha/models/slack_user.rb
+++ lib/aloha/models/slack_user.rb
@@ -1,10 +1,6 @@
 module Aloha
   class SlackUser
     def self.find(client, data)
-      logger.warn "*" * 10
-      logger.warn client.users.keys
-      logger.warn data.user_id
-      logger.warn "*" * 10
       client.users[data.user_id]
     end
   end
