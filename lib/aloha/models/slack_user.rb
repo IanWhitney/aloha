ec1b2a25883c31124d33bc94f9997f6c3b9b952a
Ian Whitney <whit0694@umn.edu>
2017-06-14 17:17:26 -0500

    debug

--- lib/aloha/models/slack_user.rb
+++ lib/aloha/models/slack_user.rb
@@ -1,7 +1,7 @@
 module Aloha
   class SlackUser
     def self.find(client, data)
-      raise "#{client.users.keys}: #{client.user}"
+      raise "#{client.users.keys}: #{data.user}"
       client.users[data.user_id]
     end
   end
