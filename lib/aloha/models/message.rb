dd07784c70d8129e61ce58f455ec3be861397f1c
Ian Whitney <whit0694@umn.edu>
2017-06-14 19:27:05 -0500

    Temporarily disable sending messages to all users
    If a user has not received a message, this code sends it to them. Even
    if they aren't new to the Slack. I don't want to accidentally spam
    people when I first deploy this or when I add a new message to the
    system.

--- lib/aloha/models/message.rb
+++ lib/aloha/models/message.rb
@@ -3,7 +3,7 @@ class Message < ActiveRecord::Base
 
   def deliver! client, user
     if user.ready_for?(self) && !user.received?(self)
-      Aloha::Server.say(client, user.username, self.content)
+      # Aloha::Server.say(client, user.username, self.content)
       Delivery.where(message: self, user: user).first_or_create!
     end
   end
