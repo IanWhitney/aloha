d5f9a2ee1c2bf448930b77eca8718d5df71dc4d7
Ian Whitney <whit0694@umn.edu>
2017-06-14 20:32:18 -0500

    Create users as they log in
    User model records are only created when the New User hook is fired. And
    messages are only delivered to users that exist in the database.
    
    If, at some point, we want to add some onboarding message that goes to
    all users, not just new users, then those users should exist in the db.

--- lib/aloha/hooks/deliver_messages.rb
+++ lib/aloha/hooks/deliver_messages.rb
@@ -6,12 +6,9 @@ module Aloha
         slack_user = Aloha::SlackUser.find(client, data)
         return if slack_user.name == client.name
 
-        user = User.find_by(slack_id: slack_user.id)
-
-        if user
-          Message.all.each do |message|
-            message.deliver!(client, user, self)
-          end
+        user = User.find_or_create_by(slack_id: slack_user.id, username: slack_user.name)
+        Message.all.each do |message|
+          message.deliver!(client, user, self)
         end
       end
     end
